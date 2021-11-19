2268f8db3ad9c8d86c013cf57edcae6e23e7c8b9
hung2504 <vanhung25042000@gmail.com>
2021-11-19 07:12:53 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import com.company.builder.Computer;
+import com.company.singleton.*;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws
+            FileNotFoundException, IOException, ClassNotFoundException {
+//        SerializedSingleton instanceOne =
+//                SerializedSingleton.getInstance();
+//        SerializedSingleton instanceTwo =
+//                SerializedSingleton.getInstance();
+
+//        EagerInitializedSingleton instanceOne =
+//                EagerInitializedSingleton.getInstance();
+//        EagerInitializedSingleton instanceTwo =
+//                EagerInitializedSingleton.getInstance();
+
+//        LazyInitializedSingleton instanceOne =
+//                LazyInitializedSingleton.getInstance();
+//        LazyInitializedSingleton instanceTwo =
+//                LazyInitializedSingleton.getInstance();
+
+//        StaticBlockSingleton instanceOne =
+//                StaticBlockSingleton.getInstance();
+//        StaticBlockSingleton instanceTwo =
+//                StaticBlockSingleton.getInstance();
+
+        ThreadSafeSingleton instanceOne =
+                ThreadSafeSingleton.getInstanceUsingDoubleLocking();
+        ThreadSafeSingleton instanceTwo =
+                ThreadSafeSingleton.getInstanceUsingDoubleLocking();
+        System.out.println("instanceOne hashCode="+instanceOne.hashCode());
+                System.out.println("instanceTwo hashCode="+instanceTwo.hashCode());
+
+
+        Computer comp = new Computer.ComputerBuilder(
+                "500 GB", "2 GB")
+                .setGraphicsCardEnabled(true).build();
+        System.out.println(comp);
+    }
+}
---
+++ src/com/company/singleton/EagerInitializedSingleton.java
@@ -0,0 +1,12 @@
+package com.company.singleton;
+
+public class EagerInitializedSingleton {
+    private static final EagerInitializedSingleton instance =
+            new EagerInitializedSingleton();
+
+    private EagerInitializedSingleton(){};
+    public static EagerInitializedSingleton getInstance(){
+        return instance;
+    }
+
+}
